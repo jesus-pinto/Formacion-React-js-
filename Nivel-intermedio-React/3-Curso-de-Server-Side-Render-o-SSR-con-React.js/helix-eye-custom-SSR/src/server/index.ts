fcbefeadefbdfa6a562a0c117f8133af1e15555d
jesuspinto <jesus.pinto@itglobers.com>
2025-10-17 15:45:33 -0400

    SSR

--- Nivel-intermedio-React/3-Curso-de-Server-Side-Render-o-SSR-con-React.js/helix-eye-custom-SSR/src/server/index.ts
+++ Nivel-intermedio-React/3-Curso-de-Server-Side-Render-o-SSR-con-React.js/helix-eye-custom-SSR/src/server/index.ts
@@ -1,10 +1,31 @@
 import express, { Express, Request, Response } from "express";
 import { config } from "./config";
+import { render } from "./render";
+import axios from "axios";
+
 
 const app: Express = express();
 
+app.use(express.static('dist'))
+
+app.get('/galaxia', async(req: Request, res:Response) => {
+  try{
+    const {data} = await axios.get("http://images-api.nasa.gov/search?q=galaxies")
+  
+    const initalProps = {
+      galacies: data?.collection?.items
+    }
+
+    return(render(req.url, initalProps))
+  } catch(error) {
+    throw new Error("El error ocurren en /galaxias", error)
+  }
+  
+  
+})
+
 app.get('/{\*todasLasRutas}', (req:Request, res:Response) => {
-  res.send(`<h1>Hola mundo desde las rutas: ${req.url}</h1>`);
+  res.send(render(req.url));
 });
 
 
